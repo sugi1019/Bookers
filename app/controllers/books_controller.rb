2d62db6493e5bcc336cd63d7283979c58c91011f
EC2 Default User <ec2-user@ip-172-31-8-143.ap-northeast-1.compute.internal>
2025-02-11 06:31:04 +0000

    [Add] ビュー.アクション追加 index.create.show.edit.update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,39 @@
 class BooksController < ApplicationController
 
   def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def create         # インデックス画面で Create Book ボタンを押したときに呼び出されるアクション
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      render :books
+    end
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+
+
+
+
+private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
